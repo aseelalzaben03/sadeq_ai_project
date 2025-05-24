3182fd7f51dbb4992424d1bfb4aa7b5856f0837b
aseelalzaben03 <167135023+aseelalzaben03@users.noreply.github.com>
2025-05-24 21:58:35 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,10 @@
+# app/initial.py
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+# اسم الريبو على HuggingFace
+model_name = "Aseelalzaben03/sadaqai-bestmodel"
+
+# تحميل الـtokenizer والموديل من HuggingFace Hub
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name, trust_remote_code=True)
 
